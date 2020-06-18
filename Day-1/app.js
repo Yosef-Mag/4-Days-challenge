4e88c5d587b5f59aa838073a2be668113d6ee213
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-06-18 17:09:32 +0300

    strgilling with js

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,10 +1,10 @@
 var test = function() {
     console.log('test')
 }
+var boxes = document.querySelectorAll('.gb')
+var player = 'X';
 
-var player1 = 'X';
-var player2 = 'O';
-var boxes = document.querySelectorAll('.gb') 
+
 
 var winningCompo = [
     [0,1,2],
@@ -17,43 +17,34 @@ var winningCompo = [
     [2,4,6]
 ];
 
-player1Turn = function() {
-    document.getElementById('?').innerHTML = player1;
-}
-player2Turn = function() {
-    document.getElementById('?').innerHTML = player2;
-}
+
 // loop over the hole bord boxes
 // in first click add x
 // second one o an so on ......
 // chick if we have three xs or os in one of the 
 // winning pos if true diclare winner
 // if not and all boxes are fill draw 
+
+
 var play = function() {
     console.log(boxes)
     for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", function () {
-        document.getElementById('b0').textContent = 'X';
-        })
+    boxes[i].addEventListener("click", function() {
+
+    }, false)
     } 
 }
 play()
 
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )
-// addEventListener("click", player1)
-// addEventListener("click", )    
+var playerHandler = function() {
+    console.log('test')
+}
+
+var switchP = function() {
+    if (player === 'X'){
+        player = 'O';
+    }
+    else {
+        player = 'X'
+    }
+}
