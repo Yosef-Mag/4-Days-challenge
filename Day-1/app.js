55d555c472cf070114cc7622958e67c62196721b
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-06-20 07:41:07 +0300

    able ato print X/O useing JQ

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,19 +1,24 @@
 var test = function() {
     console.log('test')
 }
-var boxes = document.querySelectorAll('.gb')
+
+// starting move
 var player = 'X';
 
-box0 = document.getElementById('b0');
-box1 = document.getElementById('b1');
-box2 = document.getElementById('b2');
-box3 = document.getElementById('b3');
-box4 = document.getElementById('b4');
-box5 = document.getElementById('b5');
-box6 = document.getElementById('b6');
-box7 = document.getElementById('b7');
-box8 = document.getElementById('b8');
+// calling the pos for winning fun
+var boxes = document.querySelectorAll('.gb')
+// calling the pos
+var box0 = document.getElementById('b0');
+var box1 = document.getElementById('b1');
+var box2 = document.getElementById('b2');
+var box3 = document.getElementById('b3');
+var box4 = document.getElementById('b4');
+var box5 = document.getElementById('b5');
+var box6 = document.getElementById('b6');
+var box7 = document.getElementById('b7');
+var box8 = document.getElementById('b8');
 
+// winning pos
 var winningCompo = [
     [0,1,2],
     [3,4,5],
@@ -25,98 +30,76 @@ var winningCompo = [
     [2,4,6]
 ];
 
-
-// loop over the hole bord boxes
-// in first click add x
-// second one o an so on ......
-// chick if we have three xs or os in one of the 
-// winning pos if true diclare winner
-// if not and all boxes are fill draw 
-
-
-var play = function() {
-    console.log(boxes)
-    for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", function() {
+// adding X/O 
+var play = function () {
+    box0.addEventListener("click", function() {
         box0.innerHTML = player
+        switchP();
+        winner();
+    });
+    
+    box1.addEventListener("click", function() {
         box1.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box2.addEventListener("click", function() {
         box2.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box3.addEventListener("click", function() {
         box3.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box4.addEventListener("click", function() {
         box4.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box5.addEventListener("click", function() {
         box5.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box6.addEventListener("click", function() {
         box6.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box7.addEventListener("click", function() {
         box7.innerHTML = player
+        winner();
+        switchP();
+    })
+    
+    box8.addEventListener("click", function() {
         box8.innerHTML = player
         winner();
         switchP();
-    }, false)
-    } 
-}
-play()
-
-var playerHandler = function() {
-    console.log('test')
-}
-
-var switchP = function() {
+    })
+    
+    
+var switchP = function() { 
     if (player === 'X'){
         player = 'O';
+}
+        else {
+            player = 'X'
+        }
     }
-    else {
-        player = 'X'
+    
+    var winner = function() {
+        if (box0.value === "X" && box1.value === "X" ) {
+            console.log("X Wins")
+        }
     }
 }
-
-var winner = function() {
-    if (box0.value  === 'X' && box1.value === 'X' && box2.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box0.value === 'O' && box1.value === 'O' && box2.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box3.value === 'X' && box4.value === 'X' && box5.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box3.value === 'O' && box4.value === 'O' && box5.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box6.value === 'X' && box7.value === 'X' && box8.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box6.value === 'O' && box7.value === 'O' && box8.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box0.value === 'X' && box4.value === 'X' && box8.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box0.value === 'O' && box4.value === 'O' && box8.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box0.value === 'X' && box3.value === 'X' && box6.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box0.value === 'O' && box3.value === 'O' && box6.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box1.value === 'X' && box4.value === 'X' && box7.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box1.value === 'O' && box4.value === 'O' && box7.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box2.value === 'X' && box5.value === 'X' && box8.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box2.value === 'O' && box5.value === 'O' && box8.value === 'O') {
-        alert('O Wins')
-    }
-    else if (box2.value === 'X' && box4.value === 'X' && box6.value === 'X') {
-        alert('X Wins')
-    }
-    else if (box2.value === 'O' && box4.value === 'O' && box6.value === 'O') {
-        alert('O Wins')
-    }
-    else if (document.getElementsByTagName('gb').value === true){
-        alert('Draw')
-    }
-}
+    play()
